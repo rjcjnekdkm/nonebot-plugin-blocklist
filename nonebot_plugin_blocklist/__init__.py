dc208ca568250e4b5afa0114fcbb6e134e1cb847
桜与白 <318099184@qq.com>
2022-11-05 16:59:28 +0800

    艾特中包含拦截列表时也阻止事件

--- nonebot_plugin_blocklist/__init__.py
+++ nonebot_plugin_blocklist/__init__.py
@@ -1,3 +1,4 @@
+import json
 import nonebot
 from nonebot.matcher import Matcher
 from nonebot.adapters import Event
@@ -6,7 +7,7 @@ from nonebot.adapters.onebot.v11 import GROUP_ADMIN, GROUP_OWNER
 from nonebot import get_driver, on_command, on_message, logger
 
 config = nonebot.get_driver().config
-blacklist = get_driver().config.bot_blocklist
+blocklist = get_driver().config.bot_blocklist
 
 
 user_permission = on_command("测试权限")
@@ -24,12 +25,40 @@ async def _(bot: Bot, event: Event):
             await user_permission.send("群员测试成功")
 
 
-block = on_message(priority=0, block=False)
+block_msg = on_message(priority=0, block=False)
 
 
-@block.handle()
+@block_msg.handle()
 async def _(event: Event, matcher: Matcher):
-    if blacklist and event.get_user_id() in blacklist:
-        logger.info("已忽略该指令")
-        # await block.send("Error")
+    at = At(event.json())
+    if at != []:
+        for x in blocklist:
+            if int(x) in at:
+                logger.info("已忽略该条指令")
+                matcher.stop_propagation()
+    elif event.get_user_id() in blocklist:
+        logger.info("已拦截该人指令")
         matcher.stop_propagation()
+
+
+def At(data: str):
+    """
+    检测at了谁，返回[qq, qq, qq,...]
+    包含全体成员直接返回['all']
+    如果没有at任何人，返回[]
+    :param data: event.json
+    :return: list
+    此部分来自@yzyyz1387的nonebot_plugin_admin，感谢
+    """
+    try:
+        qq_list = []
+        data = json.loads(data)
+        for msg in data["message"]:
+            if msg["type"] == "at":
+                if 'all' not in str(msg):
+                    qq_list.append(int(msg["data"]["qq"]))
+                else:
+                    return ['all']
+        return qq_list
+    except KeyError:
+        return []
